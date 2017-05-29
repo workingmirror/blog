3611dd7d6afaef1aeabe5f54b897267cbdf02023
Tim Poon <mockenoff@yahoo.com>
2017-05-28 19:04:07 -0500

    Hide/show the header based on scrolling

--- theme/static/js/main.js
+++ theme/static/js/main.js
@@ -1,5 +1,87 @@
 'use strict';
 
+// Dynamic header
+
+(function () {
+	var isTicking = false,
+		didHitTop = false,
+		shouldHideHeader = false,
+		shouldLockHeader = false,
+		lastScroll = 0,
+		lastChange = 0,
+		headerElem = document.querySelector('.header'),
+		headerHeight = headerElem.offsetHeight,
+		SCROLL_THRESHOLD = 5,
+		SHEER_CLASS = 'sheer',
+		SLIM_CLASS = 'slim',
+		LOCK_CLASS = 'lock-header',
+		HIDE_CLASS = 'hide',
+		IS_SHEER = headerElem.classList.contains(SHEER_CLASS);
+
+	function onScroll() {
+		if (isTicking !== true) {
+			isTicking = true;
+			window.requestAnimationFrame(scrollStep);
+		}
+	}
+
+	function scrollStep() {
+		isTicking = false;
+
+		var currentScroll = window.scrollY,
+			currentDelta = currentScroll - lastScroll,
+			lastDelta = currentScroll - Math.abs(lastChange);
+
+		if (currentScroll === 0) {
+			didHitTop = true;
+			shouldHideHeader = false;
+			shouldLockHeader = false;
+			lastChange = 0;
+		} else if (currentDelta > 0) {
+			if (lastChange <= 0) {
+				lastChange = currentDelta;
+			} else if (lastDelta > SCROLL_THRESHOLD && currentScroll > headerHeight) {
+				didHitTop = false;
+				shouldHideHeader = true;
+				shouldLockHeader = true;
+			}
+		} else if (currentDelta < 0) {
+			if (lastChange >= 0) {
+				lastChange = currentDelta;
+			} else if (Math.abs(lastDelta) > SCROLL_THRESHOLD) {
+				shouldHideHeader = false;
+				shouldLockHeader = true;
+			}
+		}
+
+		lastScroll = currentScroll;
+
+		if (shouldHideHeader === true) {
+			headerElem.classList.add(HIDE_CLASS);
+		} else {
+			headerElem.classList.remove(HIDE_CLASS);
+		}
+
+		if (shouldLockHeader === true) {
+			document.body.classList.add(LOCK_CLASS);
+			headerElem.classList.add(SLIM_CLASS);
+			if (IS_SHEER === true) {
+				headerElem.classList.remove(SHEER_CLASS);
+			}
+		} else {
+			document.body.classList.remove(LOCK_CLASS);
+			headerElem.classList.remove(SLIM_CLASS);
+			if (IS_SHEER === true) {
+				headerElem.classList.add(SHEER_CLASS);
+			}
+		}
+	}
+
+	window.addEventListener('scroll', onScroll);
+
+	onScroll();
+})();
+
 // Parallax backgrounds
 
 (function () {
