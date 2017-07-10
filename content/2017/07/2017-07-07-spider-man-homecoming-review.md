1fb0b99ec255ee888505283f1ed32175324bc7e3
Tim Poon <mockenoff@yahoo.com>
2017-07-10 03:51:12 -0500

    List, delete, add albums

---
+++ content/2017/07/2017-07-07-spider-man-homecoming-review.md
@@ -0,0 +1,51 @@
+Title: Spider-Man: Homecoming Review
+Date: 2017-07-07T15:58:52+00:00
+Modified: 2017-07-07T15:58:52+00:00
+Category: Reviews
+Subcategories: Movies
+Tags: Spider-Man: Homecoming, Jon Watts, Tom Holland, Michael Keaton, Marisa Tomei
+Slug: spider-man-homecoming-review
+Authors: Tim Poon
+Summary: Welcome home.
+Featured: https://workingmirror.com/blog/wp-content/uploads/2016/12/spider-man-homecoming-2.jpg
+Rating: 9
+Release: July 7, 2017
+Genre: Action-adventure science fiction
+Director: Jon Watts
+Cast: Tom Holland, Michael Keaton, Marisa Tomei
+Duration: PT2H13M
+Website: http://www.spidermanhomecoming.com/
+
+*Spider-Man: Homecoming* is a meaningful yet familiar film that tackles one of the most obvious but rare topics of superheroes: living up to the name. It sometimes creeps up in an origin story where they realize they're a hero, but it often goes wide of the mark. With *Homecoming*, we already have the experience of seeing Peter Parker (Tom Holland) in *Civil War* sling some webs, and he's been doing it long before that. No, this movie goes after the idea that this boy has to earn the Man.
+
+It's strange that this has to be such a remarkable notion considering the foundation of the character, which is to say a high school student. Sam Raimi's take thrust him squarely into the beatdown of the grownup grind while Marc Webb flashed around a school just for kicks. Writer/director Jon Watts (and a bunch of other co-writers), however, thrives on the idea that Peter is fighting both everything inside him and all that surrounds him to be what he always knew he was.
+
+There's a fascinating and complex web (ha!) of influences that inform this rendition of the classic character. His first major outing as Spider-Man was on the grandest stage possible, fighting alongside Iron Man against Captain America. Going back to being the Friendly Neighborhood Friend and helping give directions isn't giving him the same fulfillment as what he considers a proper hero. All he wants is to get back to the big boy table (via hilariously pestering Jon Favreau's pitch-perfect Happy Hogan).
+
+{% youtube U0D3AOldjMU %}
+
+But that's flying in the face of a slew of desires and pressures. Being a full-time crimefighter means tabling his crush for his classmate Liz (Laura Harrier), shortchanging his best friend Ned (Jacob Batalon), and cold shouldering his litany of near and dear extracurriculars. And you throw in Tony Stark's (Robert Downey, Jr.) insistence that he stay grounded and Aunt May's (Marisa Tomei) implicative existence, and you have Peter's environment battling what's inside of him as much as the criminals of New York do.
+
+It may sound familiar in many ways, especially since Raimi's films went after that exact same thread but hyperfocused within Mary Jane and Peter's star-crossed relationship, but Watts goes the other direction. (Same goes for a rather...*recognizable* action sequence.) Rather than fight to keep a hold on his love(s), this Spider-Man is willing to shelve all of it to keep doing what he knows he *has* to be doing. Ditch a party, eschew friendships, shirk responsibilities. Whatever the job needs.
+
+A lot of how all that works is at the commendable and young hands of Holland's performance. He is the best Peter Parker *and* Spider-Man. Tobey Maguire may have nailed the bumbling introvert aspect and Andrew Garfield was snapping off one-liners like a wall-crawling pro, but Holland has it all. He has the uncertainty of social mores that any adolescent does, but his confidence of someone who stood his ground against Steve Rogers is always simmering just beneath the surface. Excitable, anxious, brazen, shy (and, best of all, brainy and interminably kind). It works.
+
+{% img full-image https://workingmirror.com/blog/wp-content/uploads/2016/12/spider-man-homecoming-3.jpg "Spider-Man: Homecoming" "Spider-Man: Homecoming" %}
+
+It lends stellar credibility to the myriad shades of teenage complexity, replete with shades of Linklater as he stumbles his way to the end. His stubborn belief that he knows his limitations better than those that have gone through the same, a short sighted vision of his future, etc. His effortless and endless charm is obvious, but his dramatic chops get a workout, too. In one particular scene, it feels like he channels every single bit of experiential efficacy from his time on *The Impossible* into just one moment, and it is unexpectedly powerful. (And that's not to mention his [dance](https://www.instagram.com/p/BJJUFm_D0S_/) and [gymnastics](https://www.instagram.com/p/BJojNyMjE6T/) [background](https://www.youtube.com/watch?v=WmU7X_89mEU) making him the [most believable](https://www.youtube.com/watch?v=icg0TNp51uE) Spider-Man ever.)
+
+Similarly, a lot of credit goes to Michael Keaton's portrayal of Adrian Toomes, aka The Vulture. He's a whirling dervish of malice and tenderness, like a more hinged version of his character in *Birdman*. (An avian period of his career?) His deviousness saves his contractor crew as well as his family, making him a mover and shaker in this unexpected new world of aliens and monsters.
+
+Through this, he provides an excellent foil to Peter's arc. One lucky break turns a high school science whiz into one of the most powerful and most naive people on the planet. One bad break forces an otherwise trusting and easygoing father onto a bad path. Their conflicting threads are perfectly emblematic of their physical clashes, an old and brutal force of stone cold pragmatism colliding with unrelenting optimism wrapped up in itchy youth.
+
+{% img full-image https://workingmirror.com/blog/wp-content/uploads/2016/12/spider-man-homecoming-4.jpg "Spider-Man: Homecoming" "Spider-Man: Homecoming" %}
+
+The easiest way to see how the film so deftly grasps these fundamentals of storytelling and a vital understanding of these characters is how the story builds into its climax. Even non-ensemble entries into the Marvel Cinematic Universe generally involve the end of the [world](https://workingmirror.com/2016/11/04/doctor-strange-review/)/[galaxy](https://workingmirror.com/2017/05/05/guardians-of-the-galaxy-vol-2-review/). Not to spoil anything, but the resolution of this particular story is seemingly self-contained. It's perfectly scaled to the new understanding Peter has of himself and makes for a refreshing conclusion.
+
+It's invigorating, too, seeing how well the film fits into the MCU. That might even be an understatement; it rips into this grand universe and sets up shop right at the heart of it. The biggest events of the ongoing cinematic continuity also forms the biggest foundations in these character's backstories. And even the smaller components like the Stark moving the Avengers HQ upstate play a sizable role in the story. Sure, simple name-dropping is in play, but this recalls the best parts of how *Agents of S.H.I.E.L.D.* steal the ball and run with it in its seasonal arcs.
+
+Granted, for some more astute Spidey enthusiasts, some of these bits and bobs may fall flat. Nuance may save the day, but the superficial tableau of these threads have been covered before. Introspective conflict through romance, the student refusing to learn, the villainous detective. Of course, for avid comic book readers, nothing in any of these are new save for combinatorial execution, but giving how recent these Spider-Man films have both enlightened and stained our minds, it may strike you as a tad stale.
+
+{% img full-image https://workingmirror.com/blog/wp-content/uploads/2016/12/spider-man-homecoming-5.jpg "Spider-Man: Homecoming" "Spider-Man: Homecoming" %}
+
+For that small and quick peek at the expiration date, though, we are gifted something prodigious. It undoubtedly begins the [passing of the baton](https://www.polygon.com/2017/7/5/15921844/avengers-4-spider-man-homecoming-kevin-feige) to a next arc of the MCU between the progenitor in RDJ's Iron Man and the obvious vehicle in Holland's stellar Spider-Man (even including a callback flyby of the Coney Island Ferris wheel) while portraying an inspiringly diverse representation of a hero's environment we have yet to see beyond tokenism. Throw in some *choice* cameos and stupendously gleeful action scenes and you've got a winner in *Spider-Man: Homecoming*.
--- theme/static/js/admin.js
+++ theme/static/js/admin.js
@@ -6,6 +6,259 @@ import 'angular-material/angular-material.css';
 import angular from 'angular';
 import 'angular-material';
 import 'satellizer';
+import AWS from 'aws-sdk';
+import Promise from 'promise';
+
+/**
+ * Wrapper class for S3 object management, specifically for images.
+ * @class
+ */
+class Pixer {
+	/**
+	 * Set the AWS config and create an S3 connection.
+	 */
+	constructor(settings) {
+		this.settings = angular.extend({
+			bucket: 'wm-blog-post-images',
+			region: 'us-west-2',
+			poolId: 'us-west-2:f6e47da1-f45e-4577-977b-434997a4c115',
+		}, settings);
+
+		AWS.config.update({
+			region: this.settings.region,
+			credentials: new AWS.CognitoIdentityCredentials({
+				IdentityPoolId: this.settings.poolId,
+			}),
+		});
+
+		this.s3 = new AWS.S3({
+			apiVersion: '2006-03-01',
+			params: {
+				Bucket: this.settings.bucket,
+			},
+		});
+	}
+
+	/**
+	 * Get all the album names in the S3 bucket.
+	 * @param {object} [settings] - A configuration object compatible with `s3.listObjectsV2()`.
+	 * @return {Promise} Promise will resolve to a list of album names.
+	 */
+	listAlbums(settings) {
+		var params = Pixer.normalizeListParameters(settings);
+
+		return new Promise(function (fulfill, reject) {
+			params.Delimiter = '/';
+
+			this.s3.listObjectsV2(params, function (err, data) {
+				if (err) {
+					console.log('err', err);
+					reject(err);
+				} else {
+					console.log('data', data);
+					fulfill({
+						count: data.KeyCount,
+						nextToken: 'NextContinuationToken' in data ? data.NextContinuationToken : false,
+						albums: data.CommonPrefixes.map(function (currentValue) {
+							return decodeURIComponent(currentValue.Prefix.slice(0, -1));
+						}),
+					});
+				}
+			});
+		}.bind(this));
+	}
+
+	/**
+	 * Create an album in the S3 bucket.
+	 * @param {string} albumName - Name of the album to create.
+	 * @return {Promise} Promise will resolve to the album name.
+	 */
+	createAlbum(albumName) {
+		return new Promise(function (fulfill, reject) {
+			albumName = albumName.trim();
+
+			if (albumName === '') {
+				return reject({
+					Message: 'Album name cannot be empty',
+				});
+			} else if (albumName.indexOf('/') > -1) {
+				return reject({
+					Message: 'Album name cannot contain slashes',
+				});
+			}
+
+			var albumKey = encodeURIComponent(albumName) + '/';
+
+			this.s3.headObject({
+				Key: albumKey,
+			}, function (err, data) {
+				if (!err) {
+					return reject({
+						Message: 'Album ' + albumName + ' already exists',
+					});
+				} else if (err.code !== 'NotFound') {
+					return reject(err);
+				}
+
+				this.s3.putObject({
+					Key: albumKey,
+				}, function (err, data) {
+					if (err) {
+						console.log('err', err);
+						reject(err);
+					} else {
+						console.log('data', data);
+						fulfill(albumName);
+					}
+				});
+			}.bind(this));
+		}.bind(this));
+	}
+
+	/**
+	 * Get the contents of the album.
+	 * @param {string} albumName - Name of the album to fetch.
+	 * @return {Promise} Promise will resolve to a list of photo objects.
+	 */
+	viewAlbum(albumName, settings) {
+		var params = Pixer.normalizeListParameters(settings),
+			albumBucketName = this.settings.bucket;
+
+		return new Promise(function (fulfill, reject) {
+			params.Prefix = encodeURIComponent(albumName.trim()) + '//';
+
+			this.s3.listObjectsV2(params, function (err, data) {
+				if (err) {
+					console.log('err', err);
+					reject(err);
+				} else {
+					console.log('data', data);
+
+					var bucketUrl = this.request.httpRequest.endpoint.href + albumBucketName + '/';
+
+					fulfill({
+						count: data.KeyCount,
+						nextToken: 'NextContinuationToken' in data ? data.NextContinuationToken : false,
+						photos: data.Contents.map(function (photo) {
+							return bucketUrl + encodeURIComponent(photo.Key);
+						}),
+					});
+				}
+			});
+		}.bind(this));
+	}
+
+	/**
+	 * Add a photo to an album.
+	 * @param {string} albumName - Name of the album to add to.
+	 * @param {object} photo - `File` object to upload.
+	 * @return {Promise} Promise will resolve to the created S3 object.
+	 */
+	addPhoto(albumName, photo) {
+		return new Promise(function (fulfill, reject) {
+			this.s3.upload({
+				Key: encodeURIComponent(albumName.trim()) + '//' + photo.name,
+				Body: photo,
+				ACL: 'public-read',
+			}, function (err, data) {
+				if (err) {
+					console.log('err', err);
+					reject(err);
+				} else {
+					console.log('data', data);
+					fulfill(data);
+				}
+			});
+		}.bind(this));
+	}
+
+	/**
+	 * Delete a photo from an album.
+	 * @param {string} albumName - Name of the album to delete from.
+	 * @param {string} photoKey - Key of the S3 object to delete.
+	 * @return {Promise} Promise will resolve to boolean indicating deletion status.
+	 */
+	deletePhoto(albumName, photoKey) {
+		return new Promise(function (fulfill, reject) {
+			this.s3.deleteObject({
+				Key: photoKey,
+			}, function (err, data) {
+				if (err) {
+					console.log('err', err);
+					reject(err);
+				} else {
+					console.log('data', data);
+					fulfill(data.DeleteMarker);
+				}
+			});
+		}.bind(this));
+	}
+
+	/**
+	 * Delete an album.
+	 * @param {string} albumName - Name of the album to delete.
+	 * @return {Promise} Promise will resolve to boolean indicating deletion status.
+	 */
+	deleteAlbum(albumName) {
+		return new Promise(function (fulfill, reject) {
+			this.s3.listObjects({
+				Prefix: encodeURIComponent(albumName.trim()) + '/',
+			}, function (err, data) {
+				if (err) {
+					return reject(err);
+				}
+
+				this.s3.deleteObjects({
+					Delete: {
+						Objects: data.Contents.map(function (object) {
+							return {Key: object.Key};
+						}),
+						Quiet: true,
+					},
+				}, function (err, data) {
+					if (err) {
+						console.log('err', err);
+						reject(err);
+					} else {
+						console.log('data', data);
+						fulfill({
+							Deleted: data.Deleted.map(function (currentValue) {
+								return {
+									Key: currentValue.Key,
+									didDelete: currentValue.DeleteMarker,
+								};
+							}),
+							Errors: data.Errors.map(function (currentValue) {
+								return {
+									Key: currentValue.Key,
+									Message: currentValue.Message,
+								};
+							}),
+						});
+					}
+				});
+			}.bind(this));
+		}.bind(this));
+	}
+
+	static normalizeListParameters(settings) {
+		var params = {};
+
+		if (typeof settings === 'object') {
+			if ('StartAfter' in settings && typeof settings.Prefix === 'string') {
+				params.StartAfter = settings.StartAfter;
+			}
+			if ('ContinuationToken' in settings && typeof settings.Prefix === 'string') {
+				params.ContinuationToken = settings.ContinuationToken;
+			}
+			if ('Prefix' in settings && typeof settings.Prefix === 'string') {
+				params.Prefix = settings.Prefix;
+			}
+		}
+
+		return params;
+	}
+}
 
 angular.module('blogAdmin', [
 	'ngMaterial',
@@ -20,26 +273,154 @@ angular.module('blogAdmin', [
 
 	$interpolateProvider.startSymbol('[[');
 	$interpolateProvider.endSymbol(']]');
-}).controller('RootCtrl', function ($scope, $auth) {
-	$scope.isLoading = false;
+}).run(function ($rootScope) {
+	$rootScope.isLoading = false;
+
+	$rootScope.VIEW_STATE_LIST = 0;
+	$rootScope.VIEW_STATE_ALBUM = 1,
+	$rootScope.VIEW_STATE_PHOTO = 2;
+}).controller('BaseCtrl', function ($scope, $rootScope, $auth) {
 	$scope.isAuthenticated = $auth.isAuthenticated();
 
+	/**
+	 * Pops open the satellizer github OAuth window.
+	 * @return {Promise} Promise object for awaiting API response.
+	 */
 	$scope.login = function () {
-		$scope.isLoading = true;
+		$rootScope.isLoading = true;
 
-		$auth.authenticate('github').then(function (data) {
+		return $auth.authenticate('github').then(function (data) {
 			console.log('success', data);
 			$scope.isAuthenticated = true;
 		}).catch(function (data) {
 			console.log('error', data);
 			$scope.logout();
 		}).finally(function () {
-			$scope.isLoading = false;
+			$rootScope.isLoading = false;
 		});
 	};
 
+	/**
+	 * Clear our the satellizer authentication and reset the state.
+	 */
 	$scope.logout = function () {
 		$auth.logout();
 		$scope.isAuthenticated = false;
 	};
+
+	/**
+	 * Set the view to the albums list
+	 */
+	$scope.viewList = function () {
+		$rootScope.viewState = $rootScope.VIEW_STATE_LIST;
+	};
+
+	/**
+	 * Set the view to the selected album.
+	 * @param {string} albumName - Name of the album to view.
+	 */
+	$scope.viewAlbum = function (albumName) {
+		$scope.albumName = albumName;
+		$rootScope.viewState = $rootScope.VIEW_STATE_ALBUM;
+	};
+
+	$scope.viewList();
+}).controller('ListCtrl', function ($scope, $rootScope, $mdDialog, pixerService) {
+	$scope.albums = [];
+	$scope.albumCount = 0;
+	$scope.nextToken = false;
+
+	$scope.listAlbums = function (searchPrefix) {
+		$rootScope.isLoading = true;
+
+		pixerService.listAlbums({
+			Prefix: typeof searchPrefix === 'string' ? searchPrefix : null,
+		}).then(function (data) {
+			console.log('success', data);
+			$scope.albums = data.albums;
+			$scope.albumCount = data.count;
+			$scope.nextToken = data.nextToken;
+		}).catch(function (data) {
+			console.log('error', data);
+		}).finally(function () {
+			$scope.$apply(function () {
+				$rootScope.isLoading = false;
+			});
+		});
+	}
+
+	$scope.listAlbums();
+
+	$scope.createAlbum = function (albumName) {
+		$rootScope.isLoading = true;
+
+		pixerService.createAlbum(albumName).then(function (data) {
+			console.log('success', data);
+			$scope.albumCount++;
+			$scope.newAlbumName = '';
+			$scope.albums.unshift(data);
+		}).catch(function (data) {
+			console.log('error', data);
+		}).finally(function () {
+			$scope.$apply(function () {
+				$rootScope.isLoading = false;
+			});
+		});
+	};
+
+	$scope.showDeleteDialog = function (albumName) {
+		$mdDialog.show(
+			$mdDialog.confirm()
+				.title('Are you sure you want to delete the album ' + albumName + '?')
+				.textContent('This cannot be undone and all contained photos will be deleted as well.')
+				.ariaLabel('Delete album')
+				.ok('Delete album')
+				.cancel('Cancel')
+		).then(function () {
+			$scope.deleteAlbum(albumName);
+		});
+	};
+
+	$scope.deleteAlbum = function (albumName) {
+		$rootScope.isLoading = true;
+
+		pixerService.deleteAlbum(albumName).then(function (data) {
+			console.log('success', data);
+			$scope.albums.splice($scope.albums.findIndex(function (element) {
+				return element === albumName;
+			}), 1);
+			$scope.albumCount--;
+		}).catch(function (data) {
+			console.log('error', data);
+		}).finally(function () {
+			$scope.$apply(function () {
+				$rootScope.isLoading = false;
+			});
+		});
+	};
+}).controller('AlbumCtrl', function ($scope, $rootScope, $mdDialog, pixerService) {
+	$scope.photos = [];
+	$scope.photoCount = 0;
+	$scope.nextToken = false;
+
+	$scope.viewAlbum = function () {
+		$rootScope.isLoading = true;
+
+		pixerService.viewAlbum($scope.albumName).then(function (data) {
+			console.log('success', data);
+			$scope.photos = data.photos;
+			$scope.photoCount = data.count;
+			$scope.nextToken = data.nextToken;
+		}).catch(function (data) {
+			console.log('error', data);
+		}).finally(function () {
+			$scope.$apply(function () {
+				$rootScope.isLoading = false;
+			});
+		});
+	};
+
+	$scope.viewAlbum();
+}).service('pixerService', function () {
+	return new Pixer();
 });
